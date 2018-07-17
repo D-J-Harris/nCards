0423015b153e190aa807a9036fa2fd7ec361edf0
JuanPablo Ramos <jprscs@gmail.com>
2018-07-17 14:27:19 -0700

    Main view controller swipe

---
+++ nCards/Controllers/ContainerViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ContainerViewController.swift
+//  nCards
+//
+//  Created by Juan Pablo Ramos on 7/17/18.
+//  Copyright © 2018 Juan Pablo Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+	// MARK: Properties
+	
+	@IBOutlet weak var scroll: UIScrollView!
+	
+	
+	// MARK: Methods
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		let left = self.storyboard?.instantiateViewController(withIdentifier: "left") as! ContactCardViewController
+		self.addChildViewController(left)
+		self.scroll.addSubview(left.view)
+		self.didMove(toParentViewController: self)
+		
+		let middle = self.storyboard?.instantiateViewController(withIdentifier: "middle") as! CustomCameraViewController
+		self.addChildViewController(middle)
+		self.scroll.addSubview(middle.view)
+		self.didMove(toParentViewController: self)
+		
+		var middleFrame: CGRect = middle.view.frame
+		middleFrame.origin.x = self.view.frame.width
+		middle.view.frame = middleFrame
+		
+		let right = self.storyboard?.instantiateViewController(withIdentifier: "right") as! LocationsViewControllers
+		self.addChildViewController(right)
+		self.scroll.addSubview(right.view)
+		self.didMove(toParentViewController: self)
+		
+		var rightFrame: CGRect = right.view.frame
+		rightFrame.origin.x = 2 * self.view.frame.width
+		right.view.frame = rightFrame
+		
+		self.scroll.contentSize = CGSize(width: (self.view.frame.width) * 3, height: (self.view.frame.height))
+	}
+}
---
+++ nCards/Controllers/LocationsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LocationsViewController.swift
+//  nCards
+//
+//  Created by Juan Pablo Ramos on 7/17/18.
+//  Copyright © 2018 Juan Pablo Ramos. All rights reserved.
+//
+
+import UIKit
+
+class LocationsViewControllers: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
