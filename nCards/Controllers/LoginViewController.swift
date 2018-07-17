c20857514329a6d0aff48b40a2f3c01dcb140544
Ricardo Ramierz <ricardo.rmz.97@hotmail.com>
2018-07-17 15:11:03 -0700

    login flow first advance
    Firebase UI implemented, Contact class created

---
+++ nCards/Controllers/LoginViewController.swift
@@ -0,0 +1,55 @@
+//
+//  LoginViewController.swift
+//  nCards
+//
+//  Created by Ricardo Ramirez on 7/17/18.
+//  Copyright © 2018 Juan Pablo Ramos. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseUI
+import FirebaseDatabase
+
+typealias FIRUser = FirebaseAuth.User
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var btnLogin: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func btnLoginTapped(_ sender: Any) {
+        print("tap")
+        
+        guard let authUI = FUIAuth.defaultAuthUI()
+            else { return }
+        
+        authUI.delegate = self
+        
+        let authViewController = authUI.authViewController()
+        present(authViewController, animated: true)
+        
+    }
+}
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        
+        // 1
+        guard let user = authDataResult?.user
+            else { return }
+        
+        // 2
+        let userRef = Database.database().reference().child("users").child(user.uid)
+        
+        // 3
+        userRef.observeSingleEvent(of: .value, with: { (snapshot) in
+            // 4 retrieve user data from snapshot
+        })
+    }
+}
--- nCards/Models/Contact.swift
+++ nCards/Models/Contact.swift
@@ -7,7 +7,40 @@
 //
 
 import Foundation
+import FirebaseDatabase.FIRDataSnapshot
 
 class Contact {
+    
+    var name: String = ""
+    var uid: String = ""
+    var email: String = ""
+    var organization: String = ""
+    var phone: String = ""
+    var address: String = ""
+    var geoLocation: Int = 0
+    var username: String = ""
+    
+    init(uid: String, username: String, name: String, email: String, organization: String, phone: String, address: String, geoLocation: Int) {
+        
+        self.name = name
+        self.email = email
+        self.organization = organization
+        self.phone = phone
+        self.address = phone
+        self.geoLocation = geoLocation
+        self.username = username
+        self.uid = uid
+    }
+    
+    init?(snapshot: DataSnapshot) {
+        guard let dict = snapshot.value as? [String : Any],
+            let username = dict["username"] as? String
+            else { return nil }
+
+        self.uid = snapshot.key
+        self.username = username
+    }
+    
+    
 	
 }
