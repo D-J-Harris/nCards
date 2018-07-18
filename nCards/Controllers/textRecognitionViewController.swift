6f6475be74b3d8bc74240b47e56d57fe90331bba
Daniel Harris <09harrisda@gmail.com>
2018-07-18 11:03:04 -0700

    This commit adds linguistic tagger updates
    also change bundle identifier to v

--- nCards/Controllers/textRecognitionViewController.swift
+++ nCards/Controllers/textRecognitionViewController.swift
@@ -14,7 +14,7 @@ class textRecognitionViewController: UIViewController {
     
     //Initialise some variables
     lazy var vision = Vision.vision()
-    var resultsText: String = ""
+    var resultsText = ""
     
     @IBOutlet weak var testImage: UIImageView!
     @IBOutlet weak var outputText: UITextView!
@@ -28,46 +28,64 @@ class textRecognitionViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-    func detectTexts(image: UIImage?) {
+    func detectTexts(image: UIImage?){
         guard let UIImage = image else {return}
         
         //Get instance of VisionTextDetector
         let textDetector = vision.textDetector()
         
+        //remember to maybe orient UIImage to .up
+        //image.imageOrientation = .up
+        
         let image = VisionImage(image: UIImage)
         
         //Start detect
-        textDetector.detect(in: image) { (features, error)  in
+        textDetector.detect(in: image) { (features, error) in
             guard error == nil, let features = features, !features.isEmpty else {
                 //error handling
-                let errorString = error?.localizedDescription ?? "Error"
-                self.outputText.text = "On-Device text detection failed with error: \(errorString)"
+                let errorString = error?.localizedDescription ?? "Placement error string"
+                self.resultsText = "On-Device text detection failed with error: \(errorString)"
                 return
             }
             
-            self.resultsText = features.map { feature in
-                return "\(feature.text)"
-                }.joined(separator: " ")
-            print("In closure \(self.resultsText)")
-            self.outputText.text = self.resultsText
-        }
-        print("After closure \(resultsText)")
-    }
- 
-    func findContactInfo(_ allWords: String) {
-        
-        //initialise linguistic tagger instance variables
-        let schemes = NSLinguisticTagger.availableTagSchemes(forLanguage: "en")
-        let options: NSLinguisticTagger.Options = [.omitPunctuation, .omitWhitespace]
-        let tagger = NSLinguisticTagger(tagSchemes: schemes, options: Int(options.rawValue))
-        tagger.string = allWords
-        
-        let range = NSRange(location: 0, length: (allWords as NSString).length)
-
-        tagger.enumerateTags(in: range, scheme: .nameType, options: options) { tag, tokenRange, _, _  in
-            let token = (allWords as NSString).substring(with: tokenRange)
-            print("\(token): \(tag)")
+            
+            self.outputText.text = features.map { feature in
+//                let transformedRect = feature.frame.applying(self.transformMatrix())
+//                UIUtilities.addRectangle(
+//                    transformedRect,
+//                    to: self.annotationOverlayView,
+//                    color: UIColor.green
+//                )
+                return "Text: \(feature.text)"
+                }.joined(separator: "\n")
         }
     }
-
+    
+    //Transforming the image
+    private func transformMatrix() -> CGAffineTransform {
+        guard let image = testImage.image else { return CGAffineTransform() }
+        let imageViewWidth = testImage.frame.size.width
+        let imageViewHeight = testImage.frame.size.height
+        let imageWidth = image.size.width
+        let imageHeight = image.size.height
+        
+        let imageViewAspectRatio = imageViewWidth / imageViewHeight
+        let imageAspectRatio = imageWidth / imageHeight
+        let scale = (imageViewAspectRatio > imageAspectRatio) ?
+            imageViewHeight / imageHeight :
+            imageViewWidth / imageWidth
+        
+        // Image view's `contentMode` is `scaleAspectFit`, which scales the image to fit the size of the
+        // image view by maintaining the aspect ratio. Multiple by `scale` to get image's original size.
+        let scaledImageWidth = imageWidth * scale
+        let scaledImageHeight = imageHeight * scale
+        let xValue = (imageViewWidth - scaledImageWidth) / CGFloat(2.0)
+        let yValue = (imageViewHeight - scaledImageHeight) / CGFloat(2.0)
+        
+        var transform = CGAffineTransform.identity.translatedBy(x: xValue, y: yValue)
+        transform = transform.scaledBy(x: scale, y: scale)
+        return transform
+    }
+    
+    
 }
