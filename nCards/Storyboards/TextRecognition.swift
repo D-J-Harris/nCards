ab90029b19b61d68d090767046349fee05e1cfb9
Daniel Harris <09harrisda@gmail.com>
2018-07-18 23:32:43 -0700

    This commit adds most complete linguistic tagging
    name, company, email, phone (where possible)

--- nCards/Storyboards/TextRecognition.swift
+++ nCards/Storyboards/TextRecognition.swift
@@ -21,11 +21,13 @@ class TextRecognition {
     func detectTexts(image: UIImage?, completion: @escaping (String) -> ()) {
 
         guard let uiimage = image else {return}
+//        uiimage.imageOrientation = .up
         
         //Get instance of VisionTextDetector
         let textDetector = vision.textDetector()
 
         let image = VisionImage(image: uiimage)
+        
         
         //Start detect
         textDetector.detect(in: image) { (features, error) in
@@ -37,8 +39,6 @@ class TextRecognition {
                 return
             }
             
-            print(features)
-            
             let testText = features.map { feature in
                 
                 return "\(feature.text)"
@@ -47,23 +47,63 @@ class TextRecognition {
             
             self.resultsText = testText
             completion(self.resultsText)
-        } //end detect
+        }
+        //end detect
     }
     
-    func linguisticTagger(_ allWords: String) {
+    func linguisticTagger(_ input: String) {
+        
+        let tagger = NSLinguisticTagger(tagSchemes: [.nameType], options: 0)
         let options: NSLinguisticTagger.Options = [.omitWhitespace, .omitPunctuation, .joinNames]
-        let schemes = NSLinguisticTagger.availableTagSchemes(forLanguage: "en")
-        let tagger = NSLinguisticTagger(tagSchemes: schemes, options: Int(options.rawValue))
-        tagger.string = allWords
-        tagger.enumerateTags(in: NSMakeRange(0, (allWords as NSString).length), scheme: NSLinguisticTagScheme.nameTypeOrLexicalClass, options: options) { (tag, tokenRange, _, _) in
-            let token = (allWords as NSString).substring(with: tokenRange)
-            if tag?.rawValue == "OrganizationName" {
-                print("Organisation: \(token)")
-            }
-            if tag?.rawValue == "PersonalName" {
-                print("Name: \(token)")
+        let range = NSRange(location: 0, length: input.utf16.count)
+        let tags: [NSLinguisticTag] = [.personalName, .placeName, .organizationName]
+        
+        //for email case
+        var allWords = [String]()
+        allWords = input.components(separatedBy: " ")
+        
+        //Combine numbers whitespaced in cell number
+        for i in 0...allWords.count-1 {
+            if numberOfIntInString(string: allWords[i]) > 2 && numberOfIntInString(string: allWords[i+1]) > 2 {
+                allWords[i+1] = allWords[i]+allWords[i+1]
+                allWords[i] = ""
             }
         }
+        
+        //name and company
+        tagger.string = input
+        tagger.setOrthography(NSOrthography.defaultOrthography(forLanguage: "en"), range: range)
+        tagger.enumerateTags(in: range, unit: .word, scheme: .nameType, options: options) { (tag, tokenRange, stop) in
+            if let tag = tag, tags.contains(tag) {
+                let token = (input as NSString).substring(with: tokenRange)
+                if tag.rawValue == "OrganizationName" {
+                    print("Organisation: \(token)")
+                }
+                if tag.rawValue == "PersonalName" {
+                    print("Name: \(token)")
+                }
+            }
+        }
+        //number and email
+        for word in allWords {
+            if word.contains("@") {
+                print("Email: \(word)")
+            }
+            if numberOfIntInString(string: word) > 7 {
+                print("Number: \(word)")
+            }
+        }
+    }
+    
+    //supporting function
+    func numberOfIntInString(string: String) -> Int {
+        var counter = 0
+        for char in string {
+            if let _ = Int(String(char)) {
+                counter += 1
+            }
+        }
+        return counter
     }
 }
 
